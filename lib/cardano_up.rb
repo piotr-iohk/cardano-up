b5eef20e30a778f3175c229498b7cd74d09cf05b
Piotr Stachyra <piotr.stachyra@iohk.io>
2022-10-06 19:01:30 +0200

    Move errors to separate file

--- lib/cardano_up.rb
+++ lib/cardano_up.rb
@@ -7,6 +7,7 @@ require 'zlib'
 require 'zip'
 
 require "cardano_up/version"
+require "cardano_up/err"
 require "cardano_up/utils"
 require "cardano_up/tar"
 require "cardano_up/install"
@@ -90,31 +91,5 @@ module CardanoUp
   # Remove configuration
   def self.remove_configuration
     FileUtils.rm_f(@@adrestia_bundler_config)
-  end
-
-  ## Exceptions
-
-  class EnvNotSupportedError < StandardError
-    def initialize(env)
-      super("Environment '#{env}' not supported. Supported are: #{ENVS}")
-    end
-  end
-
-  class ConfigNotSetError < StandardError
-    def initialize
-      super("Config not exists at '#{CardanoUp::adrestia_bundler_config}'!")
-    end
-  end
-
-  class WalletPortError < StandardError
-    def initialize
-      super("Wallet port is not set!")
-    end
-  end
-
-  class VersionNotSupportedError < StandardError
-    def initialize(ver)
-      super("Not supported version: #{ver}. Supported are: 'latest', 'master', tag (e.g. 'v2022-08-16') or pr number ('3045')")
-    end
   end
 end
---
+++ lib/cardano_up/err.rb
@@ -0,0 +1,25 @@
+module CardanoUp
+  class EnvNotSupportedError < StandardError
+    def initialize(env)
+      super("Environment '#{env}' not supported. Supported are: #{ENVS}")
+    end
+  end
+
+  class ConfigNotSetError < StandardError
+    def initialize
+      super("Config not exists at '#{CardanoUp::adrestia_bundler_config}'!")
+    end
+  end
+
+  class WalletPortError < StandardError
+    def initialize
+      super("Wallet port is not set!")
+    end
+  end
+
+  class VersionNotSupportedError < StandardError
+    def initialize(ver)
+      super("Not supported version: #{ver}. Supported are: 'latest', 'master', tag (e.g. 'v2022-08-16') or pr number ('3045')")
+    end
+  end
+end
